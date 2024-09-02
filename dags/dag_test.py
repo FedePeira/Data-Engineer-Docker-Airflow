6852e10e93b85a9d4e882b8b4760ed9f0a276f09
FedePeira <fedepr2345@gmail.com>
2024-09-02 19:46:31 -0400

    Version 1.8 - Borrado de venv - Modificaciones en dag.py

--- dags/dag_test.py
+++ dags/dag_test.py
@@ -1,15 +1,22 @@
 from datetime import datetime
-
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
+def test_dag():
+    assert dag.dag_id == "dag_test"
+    assert dag.schedule_interval == None
+    assert len(dag.tasks) == 3
+    assert init_project.task_id == "init_project_data_creation"
+    assert airflow.__name__ == "airflow"
+    assert finish_dag.__name__ == "finish_dag"
 
 with DAG(
-    dag_id="microdesafio_semana_10",
+    dag_id="dag_test",
     start_date=datetime(2024, 1, 1),
-    schedule="0 0 * * *",
-    doc_md="microdesafio para el fin del mundo"
+    schedule=None,  
+    doc_md="prueba dag"
     ) as dag:
     
     init_project = BashOperator(
@@ -24,5 +31,8 @@ with DAG(
     def finish_dag():
         print("dag finish")
 
+    test_task = PythonOperator(
+        task_id="test_dag",
+        python_callable=test_dag)
 
-    init_project >> airflow() >> finish_dag()
+    init_project >> airflow() >> finish_dag() >> test_task
